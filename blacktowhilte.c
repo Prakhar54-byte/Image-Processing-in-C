5ef8d58d3906e3260b03c06b3ea23a8b1391414e
Prakhar54-byte <162185166+Prakhar54-byte@users.noreply.github.com>
2024-04-16 22:26:22 +0530

    Update Major_Project.c

--- blacktowhilte.c
+++ blacktowhilte.c
@@ -3,72 +3,80 @@
 #define WHITE 255
 #define BLACK 0
 
+int main()
+{
+    int choice,i,j,k;
 
-int main(){
-	do{
-		printf("Select the filter for Image.")
-		printf("1.Black to White.");
-		printf("2.Blur the Image");
-		scanf("%d",&choice);
-	}
-			
-	
-	switch(choice){
-		case 1:
-	FILE *fIn = fopen("lena512.bmp","r");				//Input File name
-	FILE *fOut = fopen("b_w1.bmp","w+");		            //Output File name
+    do
+    {
+        printf("Select the filter for Image.");
+        printf("1.Black to White.");
+        printf("2.RGB to gray");
+        scanf("%d", &choice);
+    
 
-	int i;
-	unsigned char byte[54];								//to get the image header
-	unsigned char colorTable[1024];						//to get the colortable
+    switch (choice)
+    {
+//******************************BLACK TO WHITE*****************************************
+    case 1:
+        FILE *fIn = fopen("lena512.bmp", "r"); // Input File name
+        FILE *fOut = fopen("b_w.bmp", "w+");   // Output File name
 
-	if(fIn==NULL)										// check if the input file has not been opened succesfully.
-	{										
-		printf("File does not exist.\n");
-	}
+        int i;
+        unsigned char byte[54];         // to get the image header
+        unsigned char colorTable[1024]; // to get the colortable
 
-	for(i=0;i<54;i++)									//read the 54 byte header from fIn
-	{									
-		byte[i]=getc(fIn);								
-	}
+        if (fIn == NULL) // check if the input file has not been opened succesfully.
+        {
+            printf("File does not exist.\n");
+        }
 
-	fwrite(byte,sizeof(unsigned char),54,fOut);			
+        for (i = 0; i < 54; i++) // read the 54 byte header from fIn
+        {
+            byte[i] = getc(fIn);
+        }
 
-	// extract image height, width and bitDepth from imageHeader 
-	int height = *(int*)&byte[18];
-	int width = *(int*)&byte[22];
-	int bitDepth = *(int*)&byte[28];
+        fwrite(byte, sizeof(unsigned char), 54, fOut);
 
-	printf("width: %d\n",width);
-	printf("height: %d\n",height );
+        // extract image height, width and bitDepth from imageHeader
+        int height = *(int *)&byte[18];
+        int width = *(int *)&byte[22];
+        int bitDepth = *(int *)&byte[28];
 
-	int size=height*width;								//calculate image size
+        printf("width: %d\n", width);
+        printf("height: %d\n", height);
 
-	if(bitDepth<=8)										//if ColorTable present, extract it.
-	{
-		fread(colorTable,sizeof(unsigned char),1024,fIn);
-		fwrite(colorTable,sizeof(unsigned char),1024,fOut);
-	}
+        int size = height * width; // calculate image size
 
-	unsigned char buffer[size];							//to store the image data
+        if (bitDepth <= 8) // if ColorTable present, extract it.
+        {
+            fread(colorTable, sizeof(unsigned char), 1024, fIn);
+            fwrite(colorTable, sizeof(unsigned char), 1024, fOut);
+        }
 
-	fread(buffer,sizeof(unsigned char),size,fIn);		//read image data
+        unsigned char buffer[size]; // to store the image data
 
-	for(i=0;i<size;i++)									//store 0(black) and 255(white) values to buffer 
-		{
-			buffer[i] = (buffer[i] > THRESHOLD) ? WHITE : BLACK;
-		}
-	
-	fwrite(buffer,sizeof(unsigned char),size,fOut);		//write back to the output image
+        fread(buffer, sizeof(unsigned char), size, fIn); // read image data
 
-	fclose(fIn);
-	fclose(fOut);
-	break;
-	}
-}while(choice!=6);
+        for (i = 0; i < size; i++) // store 0(black) and 255(white) values to buffer
+        {
+            buffer[i] = (buffer[i] > THRESHOLD) ? WHITE : BLACK;
+        }
+
+        fwrite(buffer, sizeof(unsigned char), size, fOut); // write back to the output image
+
+        fclose(fIn);
+        fclose(fOut);
+        break;
+//************************************ RGB TO GRAY ************************************************		
+		case 2:
+		FILE *fIN=("lena_color.bmp","r");
+		FilE *fOUT=("lena_gray.bmp","w");
+		
+		
+    }
+}
+while (choice != 6);
+    
 return 0;
 }
-
-	
-
-
